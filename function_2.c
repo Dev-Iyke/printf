c4a3b9572be29ad15ad6bc270011990749d95439
12345902 <ji.mbadiwe@stu.unizik.edu.ng>
2023-10-19 09:09:21 -0700

    updating

--- function_2.c
+++ function_2.c
@@ -415,7 +415,9 @@ int print_hexa(va_list types, char map_to[], char buffer[],
 
 	i++;
 
-	return (write_unsgnd(0, i, buffer, flags, width, precision, size))
+	return (write_unsgnd(0, i, buffer, flags, width, precision, size));
+}
+
 
 #include "main.h"
 
